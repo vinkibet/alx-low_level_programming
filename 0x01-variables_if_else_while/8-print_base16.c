ed68992c9e7d73f84ae0028c2408b3dafe359615
vinkibet <vinkibet@gmail.com>
2022-09-20 07:45:46 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int a;
+	char low;
+
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
